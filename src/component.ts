2a4918904858e6d09d44788dc72787f82e1b55d8
Will <mail@wilcof.com>
2023-03-24 19:25:38 -0400

    Rendering, properties, html and overall general development

--- src/component.ts
+++ src/component.ts
@@ -1,20 +1,35 @@
 import html from "./template.html?raw";
-
 /**
  * State shared between all sofatime components, updating any of these
  * properties should cause all components to re-render.
+ *
+ * Global state is initialized and the custom componented registered at the bottom
+ * of this file.
  */
+
 interface GlobalState {
   timezone: string;
+  // @TODO  Was this just the same as locale?
   lang: string;
   use24HourDisplay: boolean;
+  locale: string;
+  dateStyle?: "short" | "medium" | "long" | "full";
+  theme?: string;
 }
+
 class SofatimeGlobalState {
   private listeners: Function[] = [];
   private state: GlobalState;
 
   constructor(state: GlobalState) {
     this.state = state;
+  }
+
+  guessUsersLocale() {
+    this.setState({
+      timezone: Intl.DateTimeFormat().resolvedOptions().timeZone,
+      locale: navigator.languages[0],
+    });
   }
 
   setState(state: Partial<GlobalState>) {
@@ -43,29 +58,24 @@ class SofatimeGlobalState {
  */
 class Sofatime extends HTMLElement {
   shadow?: ShadowRoot;
-
   constructor() {
     super();
   }
-
   /**
    * Called when watched data attribute is changed, including on initial load
    * before connectedCallback runs.
    */
   attributeChangedCallback(name: string, oldValue: string, newValue: string) {
     if (this.shadow) {
-      //this.shadow.innerHTML = Sofatime.generateHTML(this.dataset);
       this.render(globalState.getState());
     }
   }
   /** What dataset attributes are watched for attributeChangedCallback */
   static get observedAttributes() {
     return [
-      "data-sofatime",
-      "data-format",
+      "data-start",
+      "data-end",
       "data-display-24h-toggle",
-      "data-display-select",
-      "data-display-time",
     ];
   }
 
@@ -85,34 +95,55 @@ class Sofatime extends HTMLElement {
    */
   render = (state: GlobalState) => {
     if (!this.shadow) throw "ERR: Shadow never attached to component";
-    const el = this.shadow.getElementById("global");
-    if (el) {
-      el.innerHTML = `
-      <div>
-        Global<br/>
-        ${JSON.stringify(state, null, 2).replace(/\n/g, "<br/>").replace(/\s/g, "&nbsp;")}
-      </div>
-      `;
-    }
+    const { display24toggle, start, end } = this.dataset;
 
-    const local = this.shadow.getElementById("local");
-    if (local) {
-      local.innerHTML = JSON.stringify(this.dataset, null, 2).replace(/ /g, "&nbsp;").replace(
-        /\n/g,
-        "<br/>",
-      );
-    }
+    this.displayTime("start", Sofatime.getLocaleString(start, state));
+    this.displayTime("end", Sofatime.getLocaleString(end, state));
+    return;
   };
+
+  displayTime(containerId: string, value: string) {
+    const container = this.shadow?.getElementById(containerId);
+    const time = this.shadow?.getElementById(containerId + "Time");
+    if (container && time) {
+      time.innerHTML = value;
+      if (!value) container.style.display = "none";
+      else container.style.display = "block";
+    }
+  }
+
+  static getLocaleString(iso8601: string | undefined, options: GlobalState): string {
+    if (!iso8601) return "";
+    const date = new Date(iso8601);
+    if (date.toString() == "Invalid Date") return "";
+    return date.toLocaleString(options.locale, {
+      timeZone: options.timezone,
+      dateStyle: options.dateStyle,
+    });
+  }
 
   disconnectedCallback() {
     globalState.removeEventListener(this.render);
   }
 }
-
 const globalState = new SofatimeGlobalState({
-  timezone: "",
+  timezone: "UTC",
   lang: "",
   use24HourDisplay: false,
+  locale: "en-GB",
+  dateStyle: "medium",
 });
+globalState.guessUsersLocale();
 customElements.define("sofa-time", Sofatime);
-console.log(globalState);
+window.sofatime = globalState
+
+/*
+const event = new Date(Date.UTC(2012, 11, 20, 3, 0, 0));
+["en-GB", "en-US", "ko-KR", "es-MX"].forEach((locale) =>
+  ["full", "long", "medium", "short"].forEach((dateStyle) => {
+    console.log(locale, dateStyle);
+    console.log(event.toLocaleString(locale, { timeZone: "UTC", dateStyle: dateStyle }));
+    console.log("");
+  })
+);
+*/
